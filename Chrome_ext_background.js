cdb2532a11c825ff84a4c4ecabaad982d9cfa5da
bofarrell <brendanofarrell76@gmail.com>
2014-12-10 10:47:44 +0000

    Create Chrome_ext_background.js

---
+++ Chrome_ext_background.js
@@ -0,0 +1,32 @@
+ // this background script is used to invoke desktopCapture API
+// to capture screen-MediaStream.
+
+var session = ['screen'];
+
+chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(portOnMessageHandler);
+    
+    // this one is called for each message from "content-script.js"
+    function portOnMessageHandler(message) {
+        if(message == 'recapp-get-sourceId') {
+            chrome.desktopCapture.chooseDesktopMedia(session, port.sender.tab, onAccessApproved);
+        }
+    }
+
+    // on getting sourceId
+    // "sourceId" will be empty if permission is denied.
+    function onAccessApproved(sourceId) {
+        console.log('sourceId', sourceId);
+        
+        // if "cancel" button is clicked
+        if(!sourceId || !sourceId.length) {
+            return port.postMessage('PermissionDeniedError');
+        }
+        
+        // "ok" button is clicked; share "sourceId" with the
+        // content-script which will forward it to the webpage
+        port.postMessage({
+            kodacallSourceId: sourceId
+        });
+    }
+});
